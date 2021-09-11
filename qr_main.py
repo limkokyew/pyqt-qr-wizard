923a491419dd3c1f96a36a209498a08181fa0f08
Kok Yew Lim <lim.kokyew16@gmail.com>
2021-09-11 14:21:39 +0200

    Implemented proof of concept

--- qr_main.py
+++ qr_main.py
@@ -1,16 +1,57 @@
 import qrcode
+import sys
 from pyzbar import pyzbar
-from PIL import Image
+from PIL import Image, ImageQt
+from PyQt5 import QtGui, QtWidgets, uic
 
 
-def generate_qr_simple(data, args=None):
-    qr_code = qrcode.make(data)
-    qr_code.save("test.png")
+class QRMainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(QRMainWindow, self).__init__()
+        uic.loadUi("qt_ui.ui", self)
+        self.qr_code = None
+        self.generate_qr_button.clicked.connect(self.generate_qr)
+        self.save_qr_button.clicked.connect(self.save_qr)
+        self.upload_qr_button.clicked.connect(self.upload_qr)
+        self.show()
 
-def decode_qr(img_path):
-    with Image.open(img_path) as img:
-        qr_code = pyzbar.decode(img)
-    return qr_code[0].data.decode("utf-8")
+    def generate_qr(self):
+        data = self.encode_text_field.text()
+        qr_code = qrcode.make(data)
+        self.qr_code = qr_code
+        
+        qr_img = ImageQt.ImageQt(qr_code.get_image())
+        qr_img = QtGui.QPixmap.fromImage(qr_img)
+        self.encode_image.setPixmap(qr_img)
     
-generate_qr_simple("Gosig Golden ist süß!")
-print(decode_qr("test.png"))
+    def save_qr(self):
+        if self.qr_code is not None:
+            file_name = QtWidgets.QFileDialog.getSaveFileName(
+                self, "QR-Code speichern", ".", "*.png"
+            )
+            if file_name[0]:
+                self.qr_code.save(file_name[0])
+
+    def upload_qr(self):
+        qr_file_path = QtWidgets.QFileDialog.getOpenFileName(
+            self, "QR-Code hochladen", ".", "Bilder (*.jpg *.jpeg *.png)"
+        )
+        if qr_file_path[0]:
+            with Image.open(qr_file_path[0]) as img:
+                qr_code = pyzbar.decode(img)
+                qr_img = ImageQt.ImageQt(img)
+                qr_img = QtGui.QPixmap.fromImage(qr_img)
+                self.decode_image.setPixmap(qr_img)
+            if qr_code:
+                self.decode_text_field.setText(qr_code[0].data.decode("utf-8"))
+            else:
+                self.decode_text_field.setText("Es wurde kein QR-Code erkannt.")
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = QRMainWindow()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
